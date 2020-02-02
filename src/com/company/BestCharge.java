b26cb73a2f557380fa2ae773a07e98cc8932349c
Xiao Yu <xiao.susan.yu@gmail.com>
2020-02-02 13:37:29 +0800

    [Xiao]{fix}: add menu for user to choose food

--- src/com/company/BestCharge.java
+++ src/com/company/BestCharge.java
@@ -1,80 +1,82 @@
 package com.company;
+
 import java.util.*;
 
 public class BestCharge {
-    static ArrayList<Food> userInput() {
-      Scanner input = new Scanner(System.in);
-      ArrayList<Food> order = new ArrayList<>();
-      do {
-        System.out.println("What is your dish?");
-        String foodName = input.next();
-        System.out.println("How many?");
-        int count = input.nextInt();
-        Food dish = new Food(foodName, count);
-        order.add(dish);
-        System.out.println("Anything else? yes/no");
-      } while (!input.next().equals("no"));
-      input.close();
-      return order;
-    }
+  static ArrayList<Food> userInput() {
+    Scanner input = new Scanner(System.in);
+    ArrayList<Food> order = new ArrayList<>();
+    System.out.println("menu：黄焖鸡，肉夹馍，凉皮，冰峰");
+    do {
+      System.out.println("What is your choice?");
+      String foodName = input.next();
+      System.out.println("How many?");
+      int count = input.nextInt();
+      Food dish = new Food(foodName, count);
+      order.add(dish);
+      System.out.println("Anything else? yes/no");
+    } while (!input.next().equals("no"));
+    input.close();
+    return order;
+  }
 
-    static int calculatePrice(ArrayList<Food> arr) {
-      Food[] itemList = Menu.listItems();
-      int price = 0;
-      for (Food x : arr) {
-        for (int i = 0; i < itemList.length; i++) {
-          if (x.foodName.equals(itemList[i].foodName)) {
-            price += x.count * itemList[i].price;
-          }
+  static int calculatePrice(ArrayList<Food> arr) {
+    Food[] itemList = Menu.listItems();
+    int price = 0;
+    for (Food x : arr) {
+      for (Food food : itemList) {
+        if (x.foodName.equals(food.foodName)) {
+          price += x.count * food.price;
         }
       }
-      return price;
     }
+    return price;
+  }
 
-    static int getDiscountedPrice1(int price) {
-      int discountedPrice = price;
-      if (price >= 30) {
-        discountedPrice = price - (Math.floorDiv(price, 30) * 6);
-      }
-      return discountedPrice;
+  static int getDiscountedPrice1(int price) {
+    int discountedPrice = price;
+    if (price >= 30) {
+      discountedPrice = price - (Math.floorDiv(price, 30) * 6);
     }
+    return discountedPrice;
+  }
 
-    static int getDiscountedPrice2(ArrayList<Food> order, int price) {
-      int discountedPrice = price;
-      Food[] discountedList = Menu.listPromotedItems();
-      for (Food x: order) {
-        for (int i = 0; i < discountedList.length; i++) {
-          if (x.foodName.equals(discountedList[i].foodName)) {
-            discountedPrice = price - (discountedList[i].price / 2) * x.count;
-          }
+  static int getDiscountedPrice2(ArrayList<Food> order, int price) {
+    int discountedPrice = price;
+    Food[] discountedList = Menu.listPromotedItems();
+    for (Food x : order) {
+      for (Food food : discountedList) {
+        if (x.foodName.equals(food.foodName)) {
+          discountedPrice = price - (food.price / 2) * x.count;
         }
       }
-      return discountedPrice;
     }
+    return discountedPrice;
+  }
 
-    public static void main(String[] args) {
-      ArrayList<Food> order = userInput();
-      for (Food x : order) {
-        System.out.println(x.foodName + "*" + x.count);
-      }
-      int price = calculatePrice(order);
-      System.out.println("折扣前价格：" + price + "元");
-      int discountedPrice1 = getDiscountedPrice1(price);
-      int discountedPrice2 = getDiscountedPrice2(order, price);
-      int bestCharge = price;
-      String bestPromotion = "无优惠";
-      if (discountedPrice1 < price || discountedPrice2 < price) {
-        if (discountedPrice1 <= discountedPrice2) {
-          bestCharge = discountedPrice1;
-          bestPromotion = "满30减6";
-        } else {
-          bestCharge = discountedPrice2;
-          bestPromotion = "指定商品半价：黄焖鸡，凉皮";
-        }
-      }
-      String receipt = "总价：" + bestCharge + "元，优惠方式：" + bestPromotion;
-      System.out.println(receipt);
+  public static void main(String[] args) {
+    ArrayList<Food> order = userInput();
+    for (Food x : order) {
+      System.out.println(x.foodName + "*" + x.count);
     }
+    int price = calculatePrice(order);
+    System.out.println("折扣前价格：" + price + "元");
+    int discountedPrice1 = getDiscountedPrice1(price);
+    int discountedPrice2 = getDiscountedPrice2(order, price);
+    int bestCharge = price;
+    String bestPromotion = "无优惠";
+    if (discountedPrice1 < price || discountedPrice2 < price) {
+      if (discountedPrice1 <= discountedPrice2) {
+        bestCharge = discountedPrice1;
+        bestPromotion = "满30减6";
+      } else {
+        bestCharge = discountedPrice2;
+        bestPromotion = "指定商品半价：黄焖鸡，凉皮";
+      }
+    }
+    String receipt = "总价：" + bestCharge + "元，优惠方式：" + bestPromotion;
+    System.out.println(receipt);
+  }
 
 
 }
